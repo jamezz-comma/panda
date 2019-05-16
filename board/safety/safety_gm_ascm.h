345d797f3d506913694fda9a2b41167c6a112654
AndrewC <50561935+andrewcopenpilot@users.noreply.github.com>
2019-05-16 14:12:54 -0700

    Initial commit for hacking together panda firmware that will forward messages dropping GM steering related messages

--- board/safety/safety_gm_ascm.h
+++ board/safety/safety_gm_ascm.h
@@ -1,4 +1,4 @@
-// BUS 0 is on the LKAS module (ASCM) side
+// BUS 0 is on the LKAS module (ASCM) side (50/50 this is correct)
 // BUS 2 is on the actuator (EPS) side
 
 static int gm_ascm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
@@ -6,35 +6,19 @@ static int gm_ascm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   uint32_t addr = to_fwd->RIR>>21;
 
   if (bus_num == 0) {
-
-    // do not propagate lkas messages from ascm to actuators
-    // block 0x152 and 0x154, which are the lkas command from ASCM1 and ASCM2
-    // block 0x315 and 0x2cb, which are the brake and accel commands from ASCM1
-    //if ((addr == 0x152) || (addr == 0x154) || (addr == 0x315) || (addr == 0x2cb)) {
-    if ((addr == 0x152) || (addr == 0x154)) {
-      int supercruise_on = (to_fwd->RDHR>>4) & 0x1;  // bit 36
-      if (!supercruise_on) return -1;
-    }
-
-    // on the chassis bus, the OBDII port is on the module side, so we need to read
-    // the lkas messages sent by openpilot (put on unused 0x151 ane 0x153 addrs) and send it to
-    // the actuator as 0x152 and 0x154
-    if (addr == 0x151) {
-      to_fwd->RIR = (0x152 << 21) | (to_fwd->RIR & 0x1fffff);
-    }
-    if (addr == 0x153) {
-      to_fwd->RIR = (0x154 << 21) | (to_fwd->RIR & 0x1fffff);
-    }
-
-    // brake
-    if (addr == 0x314) {
-      to_fwd->RIR = (0x315 << 21) | (to_fwd->RIR & 0x1fffff);
-    }
-
+    //Forward all messages in this direction
     return 2;
   }
 
   if (bus_num == 2) {
+    // Drop all steering + brake messages
+    //if ((addr == 241) || (addr == 384)  || (addr == 417) || (addr == 715)) {
+
+    // Drop all steering messages
+    if ((addr == 384)) {
+        return -1;
+    }
+
     return 0;
   }
 
